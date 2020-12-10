d16718284906710b8a27541b61e3f244c4f3ab0f
zj66jie <1032542505@qq.com>
2020-12-10 16:55:17 +0800

    one

---
+++ 10_class/1_class.js
@@ -0,0 +1,27 @@
+class Test {
+  constructor(name, num) {
+    //constructor是一个原生方法
+    this.name = name;
+    this.num = num;
+  }
+  testLog() {
+    console.log(this.name, this.num);
+  }
+}
+class Test2 extends Test {
+  constructor(name, num, str) {
+    super(name, num); //用super继承了 Test中的this.name = name; this.num = num;
+    this.str = str;
+  }
+  // test(name, num, str) {
+  //   super(name, num); //获得弗雷Test的this
+  //   this.str = str;
+  // }
+  testLog2() {
+    console.log(this.name, this.num, this.str);
+  }
+}
+
+export const tt = new Test();
+export const tt2 = new Test2();
+export const cc = "dd";
